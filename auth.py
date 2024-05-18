8f68ef87721dfcee171cd2784385b6e36d9e0e1f
ahnaf505 <159417805+ahnaf505@users.noreply.github.com>
2024-05-18 11:35:37 +0000

    auth.py

--- auth.py
+++ auth.py
@@ -1,6 +1,15 @@
 from database import *
+from generate import *
 import random
 
 def register_user(username, hashed_pswd):
-    
-    write_db("user_")
+    rgstr_user_id = ""
+    while True:
+        curr_user_id = gen_user_id()
+        if curr_user_id == read_db("user_id_"+curr_user_id):
+            return
+        else:
+            rgstr_user_id = curr_user_id
+            break
+    write_db("user_id_"+rgstr_user_id, username)
+    write_db("uid_username_"+username, hashed_pswd)
