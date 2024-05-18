84a3ea8ee7130e1d0625bdeb21a06d836892e03d
ahnaf505 <159417805+ahnaf505@users.noreply.github.com>
2024-05-18 11:28:24 +0000

    user id generator done

--- generate.py
+++ generate.py
@@ -1,7 +1,17 @@
 import time
 import random
+import math
+import string
+
+usr_id_length = 29
 
 def gen_user_id():
     unix_time = int(time.time())
     randint01 = random.randint(50, 50000)
-    addd_numid = unix_time + randint01
+    randint02 = random.randint(2, 50)
+    randint03 = random.randint(20, 500)
+    addd_numid = math.round(unix_time + randint01)
+    divd_numid = math.round(addd_numid / randint02)
+    multpld_numid = math.round(divd_numid * randint03)
+    fnl_user_id = ''.join(random.choices(string.ascii_lowercase + string.ascii_uppercase + multpld_numid, k=usr_id_length))
+    return str(fnl_user_id)
